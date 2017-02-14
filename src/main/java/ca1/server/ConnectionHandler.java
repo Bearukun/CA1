d9e9d018138771a954ce0b64d7b8e954beda447d
Bearukun <bearukun@gmail.com>
2017-02-14 18:26:46 +0100

    Message system working, still some bugs.

--- src/main/java/ca1/server/ConnectionHandler.java
+++ src/main/java/ca1/server/ConnectionHandler.java
@@ -8,19 +8,27 @@ import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
 
-
 public class ConnectionHandler implements Runnable {
 
+    /**
+     * Variables
+     */
     private Server server;
     private Socket connection;
-    
+
     private OutputStream output = null;
     private PrintWriter writer;
     private BufferedReader reader;
-    
+
     private boolean active = true;
     private String username = "";
 
+    /**
+     * The constructor for the ConnectionHandler.
+     *
+     * @param server
+     * @param connection
+     */
     public ConnectionHandler(Server server, Socket connection) {
 
         this.server = server;
@@ -31,10 +39,8 @@ public class ConnectionHandler implements Runnable {
     @Override
     public void run() {
 
-        
-        
         try {
-            
+
             output = connection.getOutputStream();
             InputStream input = connection.getInputStream();
 
@@ -46,21 +52,39 @@ public class ConnectionHandler implements Runnable {
             while (active) {
 
                 String line = reader.readLine();
-                String[] splitString = line.split("#", 2);
                 String command = line.substring(0, line.indexOf("#"));
                 String message = line.substring(line.indexOf("#") + 1);
+
                 switch (command) {
 
                     case "LOGIN":
 
                         //Register username in class.
                         username = message;
-                        
+
                         //Register user in hashmap, and get logged in users.
-                        String userList  = server.addUser(this);
-                        
+                        String userList = server.addUser(this);
+
                         //Need to register user, and get list
-                        writer.println("OK"+userList.substring(0, userList.length()-1));
+                        writer.println("OK" + userList.substring(0, userList.length() - 1));
+                        break;
+
+                    case "MESSAGE":
+
+                        //Check if it's a private message
+                        if (message.contains("#")) {
+
+                            String reciever = message.substring(0, message.indexOf("#"));
+                            String privMessage = message.substring(message.indexOf("#") + 1);
+
+                            server.massageUser(username, reciever, privMessage);
+
+                        } else {
+
+                            server.messageEveryone(username, message);
+
+                        }
+
                         break;
 
                     case "QUIT":
@@ -86,6 +110,7 @@ public class ConnectionHandler implements Runnable {
             try {
 
                 output.close();
+                writer.close();
                 connection.close();
 
             } catch (IOException ex) {
@@ -97,22 +122,25 @@ public class ConnectionHandler implements Runnable {
         }
 
     }
-    
-    public void sendMessage(){
-        
-        
-        
-        
-    }
-    
 
+    /**
+     * Method used to send a message to the user.
+     */
+    public void sendMessage(String message) {
+
+        writer.println(message);
+
+    }
+
+    /**
+     * Method for retrieving username.
+     *
+     * @return A string containing the username.
+     */
     public String getUsername() {
-        
+
         return username;
-        
+
     }
 
-    
-    
-    
 }
