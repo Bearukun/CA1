d108776c02e84ca6a34a4bcec90cc1be80405cce
Bearukun <bearukun@gmail.com>
2017-02-14 12:47:24 +0100

    Added more code, just as a template.

--- src/main/java/ca1/server/ConnectionHandler.java
+++ src/main/java/ca1/server/ConnectionHandler.java
@@ -1,5 +1,10 @@
 package ca1.server;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 
@@ -18,7 +23,57 @@ public class ConnectionHandler implements Runnable {
     @Override
     public void run() {
         
+               OutputStream output = connection.getOutputStream();
+        InputStream input = connection.getInputStream();
+        //Use PrintWriter instead
+        PrintWriter writer = new  PrintWriter(connection.getOutputStream(), true);
+
+        // Read whatever comes in
+        BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+        String line = reader.readLine();
+        String[] splitString = line.split("#",2);
         
+        String command = line.substring(0, line.indexOf("#"));
+        String message = line.substring(line.indexOf("#")+1);
+
+        switch (command) {
+
+            case "UPPER":
+
+                writer.println(message.toUpperCase());
+                break;
+
+            case "LOWER":
+
+                writer.println(message.toLowerCase());
+                break;
+
+            case "REVERSE":
+
+                String reversed = new StringBuilder(message).reverse().toString();
+                writer.println(reversed.substring(0, 1).toUpperCase() + reversed.substring(1));
+                break;
+
+            case "TRANSLATE":
+
+                if(message.equals("hund")){
+
+                    writer.println("dog");
+
+                }else{
+
+                    writer.println("Sorry, I don't know that word! ;___;'");
+
+                }
+
+                break;
+
+            default:
+
+                writer.println("Wrong command!");
+                break;
+
+        }
         
     }
     
