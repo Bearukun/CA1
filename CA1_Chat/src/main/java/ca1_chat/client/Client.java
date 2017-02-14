e5a38414801cacb114361552e4925c0625425d73
Bearukun <bearukun@gmail.com>
2017-02-14 10:40:03 +0100

    Uploaded basic files.

---
+++ CA1_Chat/src/main/java/ca1_chat/client/Client.java
@@ -0,0 +1,16 @@
+package ca1_chat.client;
+
+/**
+ *
+ * @author 
+ */
+public class Client {
+    
+    public static void main(String[] args) {
+        
+        
+        
+    }
+    
+    
+}
---
+++ CA1_Chat/src/main/java/ca1_chat/server/Server.java
@@ -0,0 +1,16 @@
+package ca1_chat.server;
+
+/**
+ *
+ * @author bear
+ */
+public class Server {
+    
+    public static void main(String[] args) {
+        
+        
+        
+    }
+    
+    
+}
