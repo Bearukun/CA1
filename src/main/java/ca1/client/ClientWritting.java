2cba4f7adec8af88076c916ab819c0cfd5de9f4b
RadeonXRay <ceo_titanic@msn.com>
2017-02-17 00:24:22 +0100

    Tweaks and new features to the client.

--- src/main/java/ca1/client/ClientWritting.java
+++ src/main/java/ca1/client/ClientWritting.java
@@ -1,15 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ca1.client;
 
-import ca1.server.Server;
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
@@ -20,21 +12,10 @@ import java.util.logging.Logger;
 /**
  * Class that needs to allow the user to write / communicate with others on the
  * server
- *
- * @author Ceo
  */
 public class ClientWritting implements Runnable {
 
-    /*Objective
-    1: Write data from the client to the server.
-    2: Needs to be run as a Thread through the Main Class, alongside the Reader class.
-    3: Needs to receive information regarding the server connection through the class constructor and parameters.
-    4: Thread only stops when the client stops or the server disconnects.
-    5: Threads purpose and function, is to allow the user to write information to others on the server.
-    
-    
-    
-     */
+    //Variables 'n stuff 
     private Client client;
     private static Socket clientSocket;
 
@@ -50,11 +31,14 @@ public class ClientWritting implements Runnable {
      * Constructor that recives the required connection information
      *
      * @param client Takes a Client object.
-     * @param clientConnection Takes a Socket object and uses it to establish connection with a server.
+     * @param clientConnection Takes a Socket object and uses it to establish
+     * connection with a server.
      */
     public ClientWritting(Client client, Socket clientSocket) {
+
         this.client = client;
         this.clientSocket = clientSocket;
+
     }
 
     /**
@@ -65,51 +49,43 @@ public class ClientWritting implements Runnable {
      * @throws IOException
      */
     public void sendMessage(String message) throws IOException {
-        // Write to the server
-        OutputStream output = clientSocket.getOutputStream();
-        PrintWriter writer = new PrintWriter(output);
+
         writer.println(message);
-        writer.flush();
+
     }
- 
-    
+
     /*
     Implemented Runnable Thread
-    */
+     */
     @Override
     public void run() {
 
-        
         try {
 
-            
+            output = clientSocket.getOutputStream();
+            writer = new PrintWriter(output, true);
+
             userName();
 
             while (active == true) {
-                Thread.sleep(100);
-                System.out.println("Enter Message: ");
+
                 msg = sca.nextLine();
-                
-                //Needs method for handling private messages and messages for all users
-                // /w hanni besked
-                if (msg.contains("/w")) {
-               
-                    
 
-                } else {
-                    
-                    //Send to all users, inlcluding the senders name
-                    sendMessage("MSG#" + userName + "#" + msg);
-                    
-                    //Send message to all users, not including a user name.
-                    //sendMessage("MSG#ALL" + msg);
-                    
-                    
-                }
+                if (msg.contains("/whisper")) {
 
-                if (msg.equalsIgnoreCase("QUIT")) {
+                    String[] split = msg.split(" ", 3);
+
+                    sendMessage("MSG#" + split[1] + "#" + "[Private]" + split[2]);
+
+                } else if (msg.equalsIgnoreCase("/quit")) {
 
                     active = false;
+
+                } else if (msg.equalsIgnoreCase("/list")) {
+
+                } else {
+
+                    sendMessage("MSG#ALL#" + msg);
 
                 }
 
@@ -117,9 +93,12 @@ public class ClientWritting implements Runnable {
 
             System.out.println("DISCONNECTED");
 
-        } catch (IOException | InterruptedException ex) {
+        } catch (IOException ex) {
+            
             Logger.getLogger(ClientWritting.class.getName()).log(Level.SEVERE, null, ex);
+            
         }
+        
     }
 
     /**
@@ -127,6 +106,7 @@ public class ClientWritting implements Runnable {
      */
     public synchronized void userName() {
         try {
+            
             System.out.println("Enter Username: ");
             //Awaits / Scans for the userinput
             userName = sca.nextLine();
@@ -134,7 +114,9 @@ public class ClientWritting implements Runnable {
             active = true;
 
         } catch (IOException ex) {
+            
             Logger.getLogger(ClientWritting.class.getName()).log(Level.SEVERE, null, ex);
+            
         }
 
     }
