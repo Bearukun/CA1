a86b0152bf63379be1432b91cf12995c7b63d70d
hannibal <hanni.skriver@gmail.com>
2017-02-18 10:47:58 +0100

    CA1 - Gui done

--- src/main/java/ca1/client/Client.java
+++ src/main/java/ca1/client/Client.java
@@ -1,20 +1,27 @@
 package ca1.client;
 
+import ca1.client.gui.model.GuiReader;
+import ca1.client.gui.model.GuiSender;
 import java.io.IOException;
+import java.io.Reader;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.util.Observable;
+import java.util.Observer;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class Client extends Observable{
+public class Client{
 
     //Variables
     private static ExecutorService exec = Executors.newFixedThreadPool(2);
     private Socket clientSocket;
-
+    private static boolean GuiOn = false;
+     public Boolean connected;
+    private String reciever;
+    public  GuiReader reader;
     private String host;
     private int port;
 
@@ -31,7 +38,11 @@ public class Client extends Observable{
         this.port = port;
 
     }
-
+    
+     public Client() {
+        reciever = "ALL";
+        connected  = false;
+    }
     /**
      * Main Thread that launches a new instance of the client.
      *
@@ -41,7 +52,9 @@ public class Client extends Observable{
     public static void main(String[] args) throws IOException {
 
         Client client = new Client("46.101.255.231", 8081);
+        if(GuiOn == false){
         client.startClient();
+        } 
         //46.101.255.231
         //46.101.97.122
 
@@ -71,5 +84,42 @@ public class Client extends Observable{
         }
 
     }
+     public void login(String username) throws IOException {
+        //connects to the server with username
+        clientSocket = new Socket();
+        clientSocket.connect(new InetSocketAddress("46.101.255.231", 8081));
+        connected = true;
+        //creating thread to listen from server
+        reader = new GuiReader(clientSocket.getInputStream());
+        new Thread(reader).start();
+        sendMessage("LOGIN#" + username);
+    }
 
+    public void sendMessage(String message) throws IOException {
+        // Creates new Thread and writes to the server
+        new Thread(new GuiSender(clientSocket.getOutputStream(), message)).start();
+    }
+    public void GuiOn(){
+        GuiOn = true;
+    }
+    
+    public Boolean getConnected() {
+        return connected;
+    }
+
+    public void setConnected(Boolean connected) {
+        this.connected = connected;
+    }
+
+    public String getReciever() {
+        return reciever;
+    }
+
+    public void setReciever(String reciever) {
+        this.reciever = reciever;
+    }
+    
+    public void addObserver(Observer o){
+        reader.addObserver(o);
+    }
 }
